2863ab36400bb432dc2f8943e312461619c7175b
Roland Huhn <>
2025-02-11 12:04:27 +0200

    initial commit

---
+++ src/NOMAD.Maui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace NOMAD.Maui;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ src/NOMAD.Maui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace NOMAD.Maui;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
